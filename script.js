c31557af885c38b0e805a21f263aaa993d723f05
Luke Lai <lukelai.dev@gmail.com>
2022-10-15 22:45:41 +0100

    add loading spinner when fetching data

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+
+// Show Loading
+const showLoadingSpinner = () => {
+  loader.hidden = false; //hidden element is on every html elements
+  quoteContainer.hidden = true;
+};
+
+// Hide Loading
+const hideLoadingSpinner = () => {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+};
 
 // Show New Quote
 const newQuote = (quotes) => {
@@ -18,16 +31,18 @@ const newQuote = (quotes) => {
   quote.text.length > 50
     ? quoteText.classList.add("long-quote")
     : quoteText.classList.remove("long-quote");
+  // Set Quote
   quoteText.textContent = quote.text;
-  return quote;
 };
 // Get Quotes From API
 const getQuotes = async () => {
+  showLoadingSpinner();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote(apiQuotes);
+    hideLoadingSpinner();
   } catch (err) {
     // Catch Error Here
   }
